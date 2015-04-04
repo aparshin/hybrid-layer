1ded192a9d1742cd53f5626a94ade3cdf2c0d5a7
Alexander Parshin <parshin.alexander@gmail.com>
2015-04-05 01:47:53 +0300

    Render tracks directly from zip file

--- example/render/renderTracks.js
+++ example/render/renderTracks.js
@@ -1,34 +1,33 @@
 var TrackParserManager = require('../../../gpstracks/TrackParserManager.js'),
     HybridRenderer = require('../../src/render/HybridRenderer.js'),
     fs = require('fs'),
-    glob = require('glob'),
-    Q = require('q');
+    xz = require('xz'),
+    tar = require('tar'),
+    Q = require('q'),
+    path = require('path');
 
-var gpxFolder = './data/public/',
+var gpxFile = './data/australia-oceania.tar.xz',
+// var gpxFile = './data/central-america.tar.xz',
+// var gpxFile = './data/test.tar.xz',
     CHUNK_SIZE = 200,
+    MAX_TRACKS = 0,
     MAX_ZOOM = 10;
 
-var files = glob.sync(gpxFolder + '**/*.gpx'); //.slice(0, 100),
-    curIndex = 0,
-    geomsCount = 0,
+var geomsCount = 0,
     filenames = [],
     doneAll = new Q.defer(); 
 
-var processNexChunk = function() {
-    if (curIndex >= files.length) {
-        doneAll.resolve();
-        return;
-    }
+var trackParserManager = new TrackParserManager();
 
-    var curFiles = files.slice(curIndex, curIndex + CHUNK_SIZE);
-    console.log('process chunk: ', curIndex, curFiles.length);
+var fd = fs.openSync(gpxFile, 'r');
+var inFile = fs.createReadStream(null, {fd: fd});
+var decompressor = new xz.Decompressor();
 
-    var trackParserManager = new TrackParserManager();
+var tarParser = new tar.Parse();
+var rawStream = inFile.pipe(decompressor).pipe(tarParser);
 
-    curFiles.forEach(function(fileName) {
-        var data = fs.readFileSync(fileName);
-        trackParserManager.addTrack(fileName, data);
-    });
+var parseAndRenderTracks = function() {
+    var def = new Q.defer();
 
     trackParserManager.process().done(function(tracks) {
         var geoJSON = {
@@ -39,18 +38,56 @@ var processNexChunk = function() {
             })
         };
         var hybridRenderer = new HybridRenderer(geoJSON);
-        hybridRenderer.render({maxZoom: MAX_ZOOM, loadFromFiles: true, indexShift: geomsCount}).then(function() {
-            curIndex += CHUNK_SIZE;
+        hybridRenderer.render({
+            maxZoom: MAX_ZOOM,
+            loadFromFiles: true,
+            indexShift: geomsCount
+        }).done(function() {
             geomsCount += hybridRenderer.geoms.length;
             filenames = filenames.concat(hybridRenderer.geoms.map(function(geom) {
                 return geom.filename;
             }))
-            processNexChunk();
+            def.resolve();
         })
     });
+    return def.promise;
 }
 
-processNexChunk();
-doneAll.promise.done(function() {
-    fs.writeFileSync('./result/filenames.js', JSON.stringify(filenames));
+var count = 0,
+    chunkCount = 0;
+rawStream.on('entry', function(e) {
+    if (path.extname(e.path) !== '.gpx') {
+        return;
+    }
+
+    console.log('Decompressing ', e.path, e.size);
+    var buf = new Buffer(e.size),
+        pos = 0;
+    e.on('data', function(data) {
+        data.copy(buf, pos);
+        pos += data.length;
+    }).on('end', function() {
+        console.log('Parsing ', e.path);
+        trackParserManager.addTrack(e.path, buf);
+        count++;
+        chunkCount++;
+        if (chunkCount === CHUNK_SIZE) {
+            tarParser.pause();
+            parseAndRenderTracks().done(function() {
+                console.log('resume', count, MAX_TRACKS);
+                chunkCount = 0;
+                if (MAX_TRACKS && count >= MAX_TRACKS) {
+                    console.log('destroy', count);
+                    rawStream.removeAllListeners('entry');
+                    inFile.destroy();
+                    fs.writeFileSync('./result/filenames.js', JSON.stringify(filenames));
+                }
+                tarParser.resume();
+            });
+        }
+    })
+}).on('end', function() {
+    parseAndRenderTracks().done(function() {
+        fs.writeFileSync('./result/filenames.js', JSON.stringify(filenames));
+    });
 })
